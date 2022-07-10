19272bfded082212feacd767ad7c790d9f95a6e7
krpk1900 <krpk1900@gmail.com>
2022-07-10 09:32:28 +0900

    関数コンポーネントに置き換え

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={(() => this.props.onClick())}
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}
       >
-        {this.props.value}
+        {props.value}
       </button>
-    );
-  }
+  )
 }
 
 class Board extends React.Component {
